b529c82a247f68f893138cc5c9a55495378e9e08
Julianeves01 <julia.s.neves6@aluno.senai.br>
2025-03-20 08:29:55 -0300

    finished cards

--- app/page.js
+++ app/page.js
@@ -9,9 +9,11 @@ export default function Home() {
       <Header />
       <main className={styles.main}>
         <div className={styles.article}>
-          <Card className={styles.card} title="Card 1" content= "this is card 1" />
-          <Card className={styles.card} title="Card 2" content= "this is card 2" />
-          <Card className={styles.card} title="Card 3" content= "this is card 3" />
+          <Card className={styles.card} title="Por Que a Ansiedade Não Tem Nariz?" content= "A Ansiedade, uma das novas emoções em Divertida Mente 2, foi desenhada sem nariz, o que é um detalhe interessante e simbólico. A ausência do nariz pode representar como, em momentos de ansiedade, ficamos tão preocupados que parece que até esquecemos de respirar direito. Essa característica visual é uma forma divertida e criativa de mostrar como a Ansiedade pode ser uma emoção que aparece de repente e sem aviso, capturando nossa atenção totalmente." />
+
+          <Card className={styles.card} title="Por que a Nojinho é verde?" content= "A cor verde da Nojinho é um clássico quando pensamos em coisas que não gostamos. Mas, mais do que isso, a Nojinho em Divertida Mente 2 também reflete como essa emoção nos ajuda a fazer escolhas, evitando aquilo que nos faz mal ou que simplesmente não combina conosco. Ela é uma emoção que nos protege de coisas indesejadas, ajudando Riley a navegar pelas situações sociais com mais confiança." />
+
+          <Card className={styles.card} title="Por que a Raiva só usa roupa social?" content= "Raiva está sempre vestido de forma impecável, com roupas sociais que sugerem seriedade e controle. Essa escolha reflete como o Raiva é uma emoção que busca impor ordem e respeito, mesmo quando a situação parece fora de controle. As roupas sociais do Raiva mostram que ele quer ser levado a sério, e que, apesar de seu temperamento explosivo, ele desempenha um papel importante em garantir que Riley seja ouvida e respeitada." />
         </div>
         <aside className={styles.aside}>
           <div className={styles.divertidamente}>
--- components/Header.jsx
+++ components/Header.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/Header.module.css';
 export default function Header() {
     return (
         <header className={styles.header}>
-            <h1>My Blog</h1>
+            <h1>My Blog - Curiosidades do filme Divertidamente</h1>
         </header>
     );
 }
